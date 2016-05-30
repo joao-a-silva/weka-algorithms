0634834ed45b831710902cc2030f189ca37e4860
Diego Carvalho <diego.sistemas.carvalho@gmail.com>
2016-05-30 09:05:36 -0300

    Início dos códigos do NaiveBayes

---
+++ src/mains/TestNaiveBayes.java
@@ -0,0 +1,12 @@
+package mains;
+
+import models.NaiveBayesExample;
+
+public class TestNaiveBayes {
+
+	public static void main(String[] args) {
+		NaiveBayesExample exemplo = new NaiveBayesExample();
+		
+	}
+
+}
---
+++ src/models/NaiveBayesExample.java
@@ -0,0 +1,5 @@
+package models;
+
+public class NaiveBayesExample {
+	moa.classifiers.bayes.NaiveBayes naiveBayes;
+}
